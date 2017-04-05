14b07409920991f8aa50ee4a6570ef021edcd755
Heshan Eranga <erangamx@gmail.com>
2017-04-05 23:26:44 +0530

    commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,4 +1,4 @@
-angular.module('app.controllers', [])
+angular.module('app.controllers', ['ion-place-tools'	])
   
 .controller('vipinfoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
@@ -35,8 +35,7 @@ function($scope, $ionicLoading) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-
-
+	$scope.locationChanged = function (location) { alert(location); };
 }])
    
 .controller('aboutCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
