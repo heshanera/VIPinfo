9f64b751c931364ccecd10f3087ec53170e34336
Heshan Eranga <erangamx@gmail.com>
2017-04-06 16:05:31 +0530

    commit

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,10 +1,12 @@
-angular.module('app.controllers', ['ion-place-tools'	])
+angular.module('app.controllers', [	])
   
 .controller('vipinfoCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function($scope, $ionicLoading) {
   
+
+    /*
     console.log('map', 'reached that far');
     google.maps.event.addDomListener(window, 'load', function() {
 
@@ -27,6 +29,81 @@ function($scope, $ionicLoading) {
         $scope.map = map;
     });
 
+    */
+
+    var ht = $(window).height();
+    ht -= 90;
+    $(".map-block").css({"height":ht});
+
+    $(function(){     
+        var lat = -33.8688,
+            lng = 151.2195,
+            latlng = new google.maps.LatLng(lat, lng),
+            image = 'http://www.google.com/intl/en_us/mapfiles/ms/micons/blue-dot.png'; 
+             
+        var mapOptions = {           
+                center: new google.maps.LatLng(lat, lng),           
+                zoom: 13,           
+                mapTypeId: google.maps.MapTypeId.ROADMAP         
+            },
+            map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions),
+            marker = new google.maps.Marker({
+                position: latlng,
+                map: map,
+                icon: image
+             });
+         
+        var input = document.getElementById('searchTextField');         
+        var autocomplete = new google.maps.places.Autocomplete(input, {
+            types: ["geocode"]
+        });          
+        
+        autocomplete.bindTo('bounds', map); 
+        var infowindow = new google.maps.InfoWindow(); 
+     
+        google.maps.event.addListener(autocomplete, 'place_changed', function() {
+            infowindow.close();
+            var place = autocomplete.getPlace();
+            if (place.geometry.viewport) {
+                map.fitBounds(place.geometry.viewport);
+            } else {
+                map.setCenter(place.geometry.location);
+                map.setZoom(17);  
+            }
+            
+            moveMarker(place.name, place.geometry.location);
+        });  
+        
+        $("input").focusin(function () {
+            $(document).keypress(function (e) {
+                if (e.which == 13) {
+                    infowindow.close();
+                    var firstResult = $(".pac-container .pac-item:first").text();
+                    
+                    var geocoder = new google.maps.Geocoder();
+                    geocoder.geocode({"address":firstResult }, function(results, status) {
+                        if (status == google.maps.GeocoderStatus.OK) {
+                            var lat = results[0].geometry.location.lat(),
+                                lng = results[0].geometry.location.lng(),
+                                placeName = results[0].address_components[0].long_name,
+                                latlng = new google.maps.LatLng(lat, lng);
+                            
+                            moveMarker(placeName, latlng);
+                            $("input").val(firstResult);
+                        }
+                    });
+                }
+            });
+        });
+         
+         function moveMarker(placeName, latlng){
+            marker.setIcon(image);
+            marker.setPosition(latlng);
+            infowindow.setContent(placeName);
+            infowindow.open(map, marker);
+         }
+    });
+
     
 
 }])
@@ -35,7 +112,11 @@ function($scope, $ionicLoading) {
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
-	$scope.locationChanged = function (location) { alert(location); };
+	
+	  
+
+
+
 }])
    
 .controller('aboutCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
