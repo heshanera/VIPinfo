a20d53a27f3592cf1c2978dd6e0b1614fb8a4717
Heshan Eranga <erangamx@gmail.com>
2017-05-09 07:59:40 +0530

    corrected alignments

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -378,5 +378,19 @@ angular.module('starter.controllers', [])
 
 .controller('PlaceCtrl', function($scope, $stateParams, $timeout, ionicMaterialInk, ionicMaterialMotion) {
     
+    $scope.$parent.showHeader();
+    $scope.$parent.clearFabs();
+    $scope.$parent.setHeaderFab('left');
 
+    // Delay expansion
+    $timeout(function() {
+        $scope.isExpanded = true;
+        $scope.$parent.setExpanded(true);
+    }, 300);
+
+    // Set Motion
+    ionicMaterialMotion.fadeSlideInRight();
+
+    // Set Ink
+    ionicMaterialInk.displayEffect();
 })
